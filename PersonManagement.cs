454b87b3cfb79db2b1e4db1dd2ad3e27dafbce8e
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-20 23:37:44 +0530

    [Rahmath]Add: REtreiving Data Between Age 13 To 18

--- PersonManagement.cs
+++ PersonManagement.cs
@@ -14,7 +14,7 @@ namespace PersonDataManagement
             personlist.Add(new Person { SSN = 1, Name = "Asfiya", Address = "Belur", Age = 24 });
             personlist.Add(new Person { SSN = 2, Name = "Tanveer", Address = "Hassan", Age = 34 });
             personlist.Add(new Person { SSN = 3 ,Name = "Manha", Address = "Dubai", Age = 01 });
-            personlist.Add(new Person { SSN = 4, Name = "Talha", Address = "Bangalore", Age = 13 });
+            personlist.Add(new Person { SSN = 4, Name = "Talha", Address = "Bangalore", Age = 16 });
             personlist.Add(new Person { SSN = 5, Name = "Mehwish", Address = "Delhi", Age = 22 });
             personlist.Add(new Person { SSN = 6, Name = "Mannan", Address = "Lucknow", Age = 56});
             personlist.Add(new Person { SSN = 7, Name = "Kainath", Address = "Hyderabad", Age = 67 });
@@ -33,6 +33,11 @@ namespace PersonDataManagement
         {
             var Result = this.personlist.Where(x => x.Age < 60).Take(2);
             Display(Result);
+        }
+        public void GetRecords()
+        {
+            var result = this.personlist.Where(x => x.Age > 13 && x.Age < 18);
+            Display(result);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ using PersonDataManagement;
 PersonManagement personManagement = new PersonManagement();
 personManagement.AddDefaultData();
 personManagement.GetTopRecords();
+personManagement.GetRecords();
