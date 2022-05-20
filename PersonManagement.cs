cb4d50909ad43c09ac4e0cf8dc70bbb4eb488247
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-20 23:25:50 +0530

    [Rahmath]Add: Adding Person Details Into The List

---
+++ PersonManagement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    public class PersonManagement
+    {
+        List<Person> personlist = new List<Person>();
+        public void AddDefaultData()
+        {
+            personlist.Add(new Person { SSN = 1, Name = "Asfiya", Address = "Belur", Age = 24 });
+            personlist.Add(new Person { SSN = 2, Name = "Tanveer", Address = "Hassan", Age = 34 });
+            personlist.Add(new Person { SSN = 3 ,Name = "Manha", Address = "Dubai", Age = 01 });
+            personlist.Add(new Person { SSN = 4, Name = "Talha", Address = "Bangalore", Age = 13 });
+            personlist.Add(new Person { SSN = 5, Name = "Mehwish", Address = "Delhi", Age = 22 });
+            personlist.Add(new Person { SSN = 6, Name = "Mannan", Address = "Lucknow", Age = 56});
+            personlist.Add(new Person { SSN = 7, Name = "Kainath", Address = "Hyderabad", Age = 67 });
+
+            Display(personlist);
+        }
+      
+        public void Display(IEnumerable<Person> list)
+        {
+            foreach (Person person in list) 
+            {
+                Console.WriteLine(person.SSN + " " + person.Name + " " + person.Address + " " + person.Age); ;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using PersonDataManagement;
+  
+PersonManagement personManagement = new PersonManagement();
+personManagement.AddDefaultData();
